d63f712aa64dcaa57ae8b8ae2e4c631715b22014
shockham <samuel.hockham@gmail.com>
2020-02-15 22:09:20 +0000

    Update to use warp 0.2 and thus tokio

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use warp::Filter;
 use warp::path;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // GET /hello/warp => 200 OK with body "Hello, warp!"
     let hello = path!("hello" / String)
         .map(|name| format!("Hello, {}!", name));
 
     warp::serve(hello)
-        .run(([0, 0, 0, 0], 3030));
+        .run(([0, 0, 0, 0], 3030)).await;
 }
